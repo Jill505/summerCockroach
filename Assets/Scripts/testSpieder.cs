7c0f71d138dcdd9af70e4376a0813b1c706abdf1
kguyt <123925738+kguyt@users.noreply.github.com>
2025-09-07 18:24:12 +0800

    判定增加

--- Assets/Scripts/testSpieder.cs
+++ Assets/Scripts/testSpieder.cs
@@ -7,10 +7,12 @@ public class testSpieder : MonoBehaviour
     public float damageCoolDown = 2.0f;
     public float countDown = 0f;
     public CockroachManager cManager;
+    private CameraViewToggle viewToggle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cManager = FindFirstObjectByType<CockroachManager>();
+        viewToggle = GameObject.Find("CameraManager").GetComponent<CameraViewToggle>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class testSpieder : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            if (countDown <= 0)
+            if (countDown <= 0 && !viewToggle.Is2D())
             {
                 cManager.CockroachDie();
                 countDown = damageCoolDown;
