16d9ff08beeb4f398a1468c16f490ee934da638e
kguyt <123925738+kguyt@users.noreply.github.com>
2025-09-07 17:21:55 +0800

    恐龍腳範圍圖更換

--- Assets/scripts/AK_ToolBox/DynaTracker.cs
+++ Assets/scripts/AK_ToolBox/DynaTracker.cs
@@ -9,13 +9,6 @@ public class DynaTracker : MonoBehaviour
 
     public float mySpeed = 6f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         //Cal all distance
